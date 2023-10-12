898e24768a7307f67553ec2ef7819885a8d62534
ahmadshah024 <sekandary@netlinks.af>
2023-10-12 16:39:19 +0430

    add assignment

---
+++ ems_assingment/models/ems_assignment.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+from odoo.exceptions import UserError, ValidationError
+
+
+class EmsAassignment(models.Model):
+    _name = 'ems.assignment'
+    _description = 'ems assignment description'
+
+    name = fields.Char(states={'active': [('readonly', True)]})
+    teacher_id = fields.Many2one('hr.employee', domain=[('is_teacher','=',True)], states={'active': [('readonly', True)]})
+    class_id = fields.Many2one('ems.class.room', states={'active': [('readonly', True)]})
+    subject_id = fields.Many2one('ems.subject', states={'active': [('readonly', True)]})
+    assign_date = fields.Date(default=fields.Date.today(), states={'active': [('readonly', True)]})
+    due_date = fields.Date(states={'active': [('readonly', True)]})
+    submission_type = fields.Selection([('handwritten','Handwritten'),('hardcopy','Hardcopy'),('submite','Submite')], states={'active': [('readonly', True)]})
+    file = fields.Binary('Attache Home', states={'active': [('readonly', True)]})
+    state = fields.Selection([('active','Active'),('done','Done'),('draft','Draft')], default='draft')
+
+    def action_active(self):
+        for rec in self:
+            rec.state = 'active'
+
+    def action_done(self):
+        for rec in self:
+            if rec.due_date and rec.due_date < fields.Date.today():
+                raise UserError("Cannot change state to 'done' before the due date.")
+            rec.state = 'done'
+
+
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
