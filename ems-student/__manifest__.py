c2ae865f7899c956d5e367466e5c5abb83634e35
farhadnoorzay <noorzayfarhad33@gmail.com>
2023-10-18 14:20:46 +0430

    discipline wizard added

--- ems-student/__manifest__.py
+++ ems-student/__manifest__.py
@@ -32,6 +32,7 @@
         'views/ems_parent_view.xml',
         'views/districts.xml',
         'wizards/change_school_wizard.xml',
+        'wizards/discipline_wizard.xml',
         'views/ems_class_room_view.xml',
         # 'views/ems_ClassRoom_view.xml',
         # 'views/ems_transport_view.xml',
